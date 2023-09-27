930a923ab2fa7b25e73999c99edb4b485ed0366c
Arushi <sharma.arushi17@gmail.com>
2023-09-26 22:27:20 -0700

    semantic similarity results

--- Evaluation/evaluation.py
+++ Evaluation/evaluation.py
@@ -7,6 +7,15 @@ import csv
 # Local imports
 from lm4hpc.hpcpipeline import hpcpipelines
 import evaluate
+import sys
+
+sys.argv = [
+    'your_script.py',
+    '--mcqa_dataset_file', 'rodinia-chatgpt-mcq-orig.csv',
+    '--open_ended_dataset_file', 'text.csv',
+    '--model_names', 'gpt-4', 'gpt-3.5-turbo',
+    '--output_csv', 'open-ended-text-results.csv'
+]
 
 # Constants
 OPENAI_API_KEY = "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"
@@ -19,13 +28,68 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
 # Set up external services
 import openai
 openai.api_key = os.environ["OPENAI_API_KEY"]
-print(openai.api_key)
+
 def bleu_score_eval():
     # Load metrics
     bleu = evaluate.load("bleu")
     rouge = evaluate.load("rouge")
     perplexity = evaluate.load("perplexity", module_type="metric")
     # results = perplexity.compute(predictions=predictions, model_id='gpt2')
+
+def exact_match_evaluation(mcqa_dataset,model_name):
+    '''Adapted from DoctorGPT exact match method for MCQ evaluation--removed conversation history'''
+
+    correct = 0
+    total = 0
+    OMP_QA_sc = hpcpipelines(task="openmp_question_answering", model=model_name, pdf_files="", langchain_embedding="")
+
+    results = []
+
+    for idx, example in enumerate(mcqa_dataset['train']):
+        question = example['startphrase (context + question)']
+
+        try:
+            options = "A." + example['ending0'] + "B." + example['ending1'] + "C." + example['ending2'] + "D." + example['ending3']
+            input_sample = question + options + "Please output the correct option A,B,C or D only"
+            response = OMP_QA_sc(input_sample)
+            generated_text = response
+            correct_answer = example['Answer']
+            predicted_answer_idx = generated_text[0]
+
+            is_correct = (correct_answer == predicted_answer_idx)
+            if is_correct:
+                correct += 1
+
+            results.append((model_name, question, generated_text, correct_answer, is_correct))
+            total += 1
+
+        # except ServiceUnavailableError:
+        #     print("Service unavailable. Waiting for 10 seconds before retrying...")
+        #     time.sleep(10)
+        #     continue
+
+        except TypeError:
+            print(f"Found TypeError (probably due to None value) in line {idx + 1}. Endings: {example['ending0'], example['ending1'], example['ending2'], example['ending3']}")
+
+    return correct / total, results
+
+def run_exact_match(mcqa_dataset,models):
+    # Evaluate each model
+    for model_name in models:
+        print(f"Evaluating model: {model_name}")
+        accuracy, evaluation_results = exact_match_evaluation(mcqa_dataset,model_name)
+        print(f"Accuracy for {model_name}: {accuracy * 100:.2f}%")
+
+        # Write results to CSV
+        file_exists = os.path.isfile('evaluation_results_rodinia.csv')
+        with open('evaluation_results_rodinia.csv', 'a', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+
+            if not file_exists:
+                csvwriter.writerow(['Model Name', 'Question', 'Response', 'Correct Answer', 'Is Correct'])
+
+            for result in evaluation_results:
+                csvwriter.writerow(result)
 
 def semantic_similarity_eval(open_ended_dataset,model_name, num_rows=2):
     print("entered semantic similarity function")
@@ -76,17 +140,17 @@ def semantic_similarity_eval(open_ended_dataset,model_name, num_rows=2):
     # Print the accuracy
     accuracy = correct_count / num_rows * 100
     print(f"Accuracy on the first {num_rows} rows: {accuracy}%")
-    
+
     # Return both accuracy and the list of results
     return correct_count / num_rows, results
 
-def write_results_to_csv(results, model_name, file_name='semantic_similarity_results.csv'):
+def write_ss_results_to_csv(results, model_name, file_name='semantic_similarity_results.csv'):
     '''Function to write the evaluation results to a CSV file'''
     file_exists = os.path.isfile(file_name)
-    
+
     with open(file_name, 'a', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-        
+
         # Write headers only if the file is new
         if not file_exists:
             headers = ["Model Name", "Question", "Response", "Correct Answer", "Cosine Similarity", "Is Correct"]
@@ -96,12 +160,22 @@ def write_results_to_csv(results, model_name, file_name='semantic_similarity_res
         for result in results:
             csvwriter.writerow([model_name, result["question"], result["response"], result["correct_answer"], result["similarity"], result["is_correct"]])
 
+def run_semantic_similarity(num_rows,models,num_rows):
+    # Use provided list of models
+    for model_name in args.models:
+        print(f"Evaluating model: {model_name}")
+        accuracy, results = semantic_similarity_eval(open_ended_dataset, model_name, num_rows)
+        print(f"Accuracy on the first {num_rows} rows: {accuracy * 100:.2f}%")
+
+        # Write results to CSV using provided name and model_name
+        write_ss_results_to_csv(results, model_name, args.output_csv)
+
 if __name__ == "__main__":
     # Set up the argument parser
     parser = argparse.ArgumentParser(description="Evaluate models for semantic similarity.")
     parser.add_argument("--mcqa_dataset_file", default="mcq-single-orig.csv", help="Path to the MCQA dataset file.")
     parser.add_argument("--open_ended_dataset_file", default="code.csv", help="Path to the open-ended dataset file.")
-    parser.add_argument("--model_names", nargs='+', default=["gpt-4", "gpt-3.5-turbo", "HuggingFaceH4/starchat-alpha" ], help="List of model names to evaluate.")
+    parser.add_argument("--model_names", nargs='+', default=["gpt-4", "gpt-3.5-turbo"], help="List of model names to evaluate.")
     parser.add_argument("--output_csv", default="semantic_similarity_results.csv", help="Name of the output CSV file.")
 
     # Parse the arguments
@@ -115,15 +189,8 @@ if __name__ == "__main__":
     num_rows = open_ended_dataset['train'].num_rows
     print("num_rows", num_rows)
     print(open_ended_dataset)
-    
-    # Use provided list of models
-    for model_name in args.model_names:
-        print(f"Evaluating model: {model_name}")
-        accuracy, results = semantic_similarity_eval(open_ended_dataset, model_name, num_rows)
-        print(f"Accuracy on the first {num_rows} rows: {accuracy * 100:.2f}%")
-        
-        # Write results to CSV using provided name and model_name
-        write_results_to_csv(results, model_name, args.output_csv)
 
+    run_semantic_similarity(open_ended_dataset, args.model_names, num_rows)
+    run_exact_match(mcqa_dataset,args.model_names)
 
 
--- Evaluation/load_sentence_transformer.py
+++ Evaluation/load_sentence_transformer.py
@@ -5,6 +5,6 @@ model_name = 'sentence-transformers/paraphrase-MiniLM-L6-v2'
 model = SentenceTransformer(model_name)
 
 # Save the model locally
-local_path = './local_model'
+local_path = '$PSCRATCH'
 model.save(local_path)
 
