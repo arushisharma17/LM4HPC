744950637212b89d61155d2d9526f5c5d6ec4700
Arushi Sharma <28835447+arushisharma17@users.noreply.github.com>
2023-09-27 23:00:06 -0700

    combined evaluation script

--- Evaluation/evaluation.py
+++ Evaluation/evaluation.py
@@ -9,14 +9,6 @@ from lm4hpc.hpcpipeline import hpcpipelines
 import evaluate
 import sys
 
-sys.argv = [
-    'your_script.py',
-    '--mcqa_dataset_file', 'rodinia-chatgpt-mcq-orig.csv',
-    '--open_ended_dataset_file', 'text.csv',
-    '--model_names', 'HuggingFaceH4/starchat-alpha', 'gpt-4', 'gpt-3.5-turbo',
-    '--output_csv', 'open-ended-text-results.csv'
-]
-
 # Set up external services
 import openai
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -61,25 +53,8 @@ def exact_match_evaluation(mcqa_dataset,model_name):
 
     return correct / total, results
 
-def run_exact_match(mcqa_dataset,models):
-    # Evaluate each model
-    for model_name in models:
-        print(f"Evaluating model: {model_name}")
-        accuracy, evaluation_results = exact_match_evaluation(mcqa_dataset,model_name)
-        print(f"Accuracy for {model_name}: {accuracy * 100:.2f}%")
-
-        # Write results to CSV
-        file_exists = os.path.isfile('evaluation_results_rodinia.csv')
-        with open('evaluation_results_rodinia.csv', 'a', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile)
-
-            if not file_exists:
-                csvwriter.writerow(['Model Name', 'Question', 'Response', 'Correct Answer', 'Is Correct'])
-
-            for result in evaluation_results:
-                csvwriter.writerow(result)
-
-def semantic_similarity_eval(open_ended_dataset,model_name, num_rows=2):
+def semantic_similarity_eval(open_ended_dataset,model_name):
+    '''Adapted from DoctorGPT exact match method for MCQ evaluation--removed conversation history'''
     print("entered semantic similarity function")
     correct_count = 0
 
@@ -124,61 +99,180 @@ def semantic_similarity_eval(open_ended_dataset,model_name, num_rows=2):
             "similarity": cosine_similarity,
             "is_correct": is_correct
         })
-
+    
     # Print the accuracy
+    num_rows = open_ended_dataset['train'].num_rows
+    print("num_rows", num_rows)
     accuracy = correct_count / num_rows * 100
     print(f"Accuracy on the first {num_rows} rows: {accuracy}%")
 
     # Return both accuracy and the list of results
     return correct_count / num_rows, results
 
-def write_ss_results_to_csv(results, model_name, file_name='semantic_similarity_results.csv'):
-    '''Function to write the evaluation results to a CSV file'''
-    file_exists = os.path.isfile(file_name)
 
+
+def evaluate_models(models, evaluation_type, file_name, dataset_type, args):
+    # Determine dataset files based on dataset_type
+    if dataset_type == "open_ended":
+        dataset_files = args.open_ended_dataset_file
+    elif dataset_type == "mcqa":
+        dataset_files = args.mcqa_dataset_file
+    else:
+        raise ValueError(f"Invalid dataset_type: {dataset_type}")
+
+    # Loop through dataset files
+    for data_file in dataset_files:
+        print(f"Loading data from: {data_file}")
+        
+        # Load the dataset
+        if dataset_type == "open_ended":
+            dataset = load_dataset("sharmaarushi17/HPCPerfOpt-Open-ended", data_files=data_file)
+            num_rows = dataset['train'].num_rows
+            print("num_rows", num_rows)
+            print(dataset)
+        elif dataset_type == "mcqa":
+            dataset = load_dataset("sharmaarushi17/HPCPerfOpt-MCQA", data_files=data_file)
+            print(dataset)
+
+        # Now perform evaluations
+        for model_name in models:
+            print(f"Evaluating model: {model_name}")
+
+            # Determine which evaluation method to use
+            if evaluation_type == "semantic_similarity":
+                accuracy, results = semantic_similarity_eval(dataset, model_name)
+                headers = ["Model Name", "Dataset", "Question", "Response", "Correct Answer", "Cosine Similarity", "Is Correct"]
+            elif evaluation_type == "exact_match":
+                accuracy, results = exact_match_evaluation(dataset, model_name)
+                headers = ['Model Name', 'Dataset', 'Question', 'Response', 'Correct Answer', 'Is Correct']
+            else:
+                raise ValueError(f"Invalid evaluation_type: {evaluation_type}")
+
+            print(f"Accuracy for {model_name}: {accuracy * 100:.2f}%")
+
+            # Write results to CSV
+            file_exists = os.path.isfile(file_name)
+            with open(file_name, 'a', newline='') as csvfile:
+                csvwriter = csv.writer(csvfile)
+                
+                # Write headers only if the file is new
+                if not file_exists:
+                    csvwriter.writerow(headers)
+
+                if evaluation_type == "semantic_similarity":
+                    for result in results:
+                        csvwriter.writerow([model_name, data_file, result["question"], result["response"], result["correct_answer"], result["similarity"], result["is_correct"]])
+                else:
+                    for result in results:
+                        csvwriter.writerow([model_name, data_file, result["question"], result["response"], result["correct_answer"], result["is_correct"]])
+
+# Assuming you've parsed args using argparse before calling this function.
+# Example Usage:
+# evaluate_models(["gpt-4", "gpt-3.5-turbo"], "semantic_similarity", "semantic_similarity_results.csv", "open_ended", args)
+# evaluate_models(["gpt-4", "gpt-3.5-turbo"], "exact_match", "evaluation_results_rodinia.csv", "mcqa", args)
+
+import os
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer, util
+import argparse
+import csv
+
+# Local imports
+from lm4hpc.hpcpipeline import hpcpipelines
+import sys
+
+# Set up external services
+import openai
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+def evaluate_models(models, evaluation_type, file_name, dataset_type, args):
+    # Fetch dataset files using helper function
+    dataset_files = get_dataset_files(dataset_type, args)
+
+    # Loop through dataset files
+    for data_file in dataset_files:
+        print(f"Loading data from: {data_file}")
+
+        # Load dataset data using helper function
+        dataset = load_dataset_data(dataset_type, data_file)
+
+        # Now perform evaluations
+        for model_name in models:
+            print(f"Evaluating model: {model_name}")
+
+            # Determine which evaluation method to use
+            if evaluation_type == "semantic_similarity":
+                accuracy, results = semantic_similarity_eval(dataset, model_name)
+                headers = ["Model Name", "Dataset", "Question", "Response", "Correct Answer", "Cosine Similarity", "Is Correct"]
+            elif evaluation_type == "exact_match":
+                accuracy, results = exact_match_evaluation(dataset, model_name)
+                headers = ['Model Name', 'Dataset', 'Question', 'Response', 'Correct Answer', 'Is Correct']
+            else:
+                raise ValueError(f"Invalid evaluation_type: {evaluation_type}")
+
+            print(f"Accuracy for {model_name}: {accuracy * 100:.2f}%")
+
+            # Write results to CSV using helper function
+            write_results_to_csv(results, headers, model_name, data_file, file_name)
+
+
+def get_dataset_files(dataset_type, args):
+    """Determine the dataset files based on the dataset type."""
+    if dataset_type == "open_ended":
+        return args.open_ended_dataset_file
+    elif dataset_type == "mcqa":
+        return args.mcqa_dataset_file
+    else:
+        raise ValueError(f"Invalid dataset_type: {dataset_type}")
+
+def load_dataset_data(dataset_type, data_file):
+    """Load dataset based on dataset type and file."""
+    if dataset_type == "open_ended":
+        dataset = load_dataset("sharmaarushi17/HPCPerfOpt-Open-ended", data_files=data_file)
+        print("Number of rows:", dataset['train'].num_rows)
+    elif dataset_type == "mcqa":
+        dataset = load_dataset("sharmaarushi17/HPCPerfOpt-MCQA", data_files=data_file)
+    else:
+        raise ValueError(f"Invalid dataset_type: {dataset_type}")
+    print(dataset)
+    return dataset
+
+def write_results_to_csv(results, headers, model_name, data_file, file_name):
+    """Write the evaluation results to a CSV file."""
+    file_exists = os.path.isfile(file_name)
     with open(file_name, 'a', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
 
         # Write headers only if the file is new
         if not file_exists:
-            headers = ["Model Name", "Question", "Response", "Correct Answer", "Cosine Similarity", "Is Correct"]
             csvwriter.writerow(headers)
 
-        # Write results to CSV
-        for result in results:
-            csvwriter.writerow([model_name, result["question"], result["response"], result["correct_answer"], result["similarity"], result["is_correct"]])
+        if headers[-1] == "Is Correct":  # This is a simple check to determine the type of evaluation
+            for result in results:
+                csvwriter.writerow([model_name, data_file, result["question"], result["response"], result["correct_answer"], result["is_correct"]])
+        else:
+            for result in results:
+                csvwriter.writerow([model_name, data_file, result["question"], result["response"], result["correct_answer"], result["similarity"], result["is_correct"]])
 
-def run_semantic_similarity(open_ended_dataset, models,num_rows):
-    # Use provided list of models
-    for model_name in args.model_names:
-        print(f"Evaluating model: {model_name}")
-        accuracy, results = semantic_similarity_eval(open_ended_dataset, model_name, num_rows)
-        print(f"Accuracy on the first {num_rows} rows: {accuracy * 100:.2f}%")
-
-        # Write results to CSV using provided name and model_name
-        write_ss_results_to_csv(results, model_name, args.output_csv)
 
 if __name__ == "__main__":
     # Set up the argument parser
-    parser = argparse.ArgumentParser(description="Evaluate models for semantic similarity.")
-    parser.add_argument("--mcqa_dataset_file", default="mcq-single-orig.csv", help="Path to the MCQA dataset file.")
-    parser.add_argument("--open_ended_dataset_file", default="code.csv", help="Path to the open-ended dataset file.")
-    parser.add_argument("--model_names", nargs='+', default=["gpt-4", "gpt-3.5-turbo"], help="List of model names to evaluate.")
-    parser.add_argument("--output_csv", default="semantic_similarity_results.csv", help="Name of the output CSV file.")
+    parser = argparse.ArgumentParser(description="Evaluate models for semantic similarity and exact match.")
+
+    # Multiple dataset files can be input as a list separated by commas
+    parser.add_argument("--mcqa_dataset_file", nargs='+', default=["mcq-single-orig.csv", "rodinia-chatgpt-mcq-orig.csv"], help="Paths to the MCQA dataset files.")
+    parser.add_argument("--open_ended_dataset_file", nargs='+', default=["code.csv", "text.csv"], help="Paths to the open-ended dataset files.")
+    parser.add_argument("--model_names", nargs='+', default=["HuggingFaceH4/starchat-alpha","gpt-4", "gpt-3.5-turbo"], help="List of model names to evaluate.")
+    parser.add_argument("--semantic_similarity_output_csv", default="semantic_similarity_results.csv", help="Name of the output CSV file for semantic similarity evaluation.")
+    parser.add_argument("--exact_match_output_csv", default="evaluation_results_rodinia.csv", help="Name of the output CSV file for exact match evaluation.")
 
     # Parse the arguments
     args = parser.parse_args()
 
-    # Load datasets using provided paths
-    mcqa_dataset = load_dataset("sharmaarushi17/HPCPerfOpt-MCQA", data_files=args.mcqa_dataset_file)
-    print(mcqa_dataset)
+    # Print the OpenAI API Key for debugging (consider removing this for security reasons!)
+    print(openai.api_key)
 
-    open_ended_dataset = load_dataset("sharmaarushi17/HPCPerfOpt-Open-ended", data_files=args.open_ended_dataset_file)
-    num_rows = open_ended_dataset['train'].num_rows
-    print("num_rows", num_rows)
-    print(open_ended_dataset)
-
-    run_semantic_similarity(open_ended_dataset, args.model_names, num_rows)
-    run_exact_match(mcqa_dataset,args.model_names)
-
+    # Run evaluations
+    evaluate_models(args.model_names, "semantic_similarity", args.semantic_similarity_output_csv, "open_ended", args)
+    evaluate_models(args.model_names, "exact_match", args.exact_match_output_csv, "mcqa", args)
 
